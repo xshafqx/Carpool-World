427a5bbb1232a91cbb9128d2fb8f23d801ea34ce
Shafiq <58117391+xshafqx@users.noreply.github.com>
2020-01-30 19:09:50 +0800

    Update App.js
    can go to different tabs now

--- Carpool-World/App.js
+++ Carpool-World/App.js
@@ -5,7 +5,10 @@ import { createStackNavigator } from 'react-navigation-stack';
 
 var userInfo = {user: "", pass: "", name: ""};
 
-class HomeScreen extends React.Component {
+class MainScreen extends React.Component {
+
+  ///// f u n c t i o n s /////
+
   // function to check user name and password
   // current username is user, password is 123
   checkUser = () => {
@@ -14,14 +17,73 @@ class HomeScreen extends React.Component {
     userInfo.name = "Kambing";
 
     if (userInfo.user == 'user' && userInfo.pass == '123') {
-      this.props.navigation.navigate('LoggedIn');
+      //this.props.navigation.navigate('CurrLoc');
       document.getElementById('logInSpace').style.display = "none";
-      document.getElementById('afterLogIn').style.display = "block";
+      document.getElementById('homePage').style.display = "block";
+      document.getElementById('bookPage').style.display = "none";
+      document.getElementById('msgsPage').style.display = "none";
+      document.getElementById('acctPage').style.display = "none";
       document.getElementById('welcomeMsg').innerHTML = "Welcome back, " + userInfo.name + "\n\n";
     }
     else {
       alert(userInfo.user + " " + userInfo.pass + "\nWrong username/password");
     }
+  }
+
+  // home page button
+  homePageButton = () => {
+    document.getElementById('logInSpace').style.display = "none";
+    document.getElementById('homePage').style.display = "block";
+    document.getElementById('bookPage').style.display = "none";
+    document.getElementById('msgsPage').style.display = "none";
+    document.getElementById('acctPage').style.display = "none";
+
+    document.getElementById('homeButton').disabled = true;
+    document.getElementById('bookButton').disabled = false;
+    document.getElementById('msgsButton').disabled = false;
+    document.getElementById('acctButton').disabled = false;
+  }
+
+  // bookings page button
+  bookPageButton = () => {
+    document.getElementById('logInSpace').style.display = "none";
+    document.getElementById('homePage').style.display = "none";
+    document.getElementById('bookPage').style.display = "block";
+    document.getElementById('msgsPage').style.display = "none";
+    document.getElementById('acctPage').style.display = "none";
+
+    document.getElementById('homeButton').disabled = false;
+    document.getElementById('bookButton').disabled = true;
+    document.getElementById('msgsButton').disabled = false;
+    document.getElementById('acctButton').disabled = false;
+  }
+
+  // messages page button
+  msgsPageButton = () => {
+    document.getElementById('logInSpace').style.display = "none";
+    document.getElementById('homePage').style.display = "none";
+    document.getElementById('bookPage').style.display = "none";
+    document.getElementById('msgsPage').style.display = "block";
+    document.getElementById('acctPage').style.display = "none";
+
+    document.getElementById('homeButton').disabled = false;
+    document.getElementById('bookButton').disabled = false;
+    document.getElementById('msgsButton').disabled = true;
+    document.getElementById('acctButton').disabled = false;
+  }
+
+  // account page button
+  acctPageButton = () => {
+    document.getElementById('logInSpace').style.display = "none";
+    document.getElementById('homePage').style.display = "none";
+    document.getElementById('bookPage').style.display = "none";
+    document.getElementById('msgsPage').style.display = "none";
+    document.getElementById('acctPage').style.display = "block";
+
+    document.getElementById('homeButton').disabled = false;
+    document.getElementById('bookButton').disabled = false;
+    document.getElementById('msgsButton').disabled = false;
+    document.getElementById('acctButton').disabled = true;
   }
 
   // function to log user out and clear username and password
@@ -33,8 +95,13 @@ class HomeScreen extends React.Component {
     document.getElementById('username').value = "";
     document.getElementById('password').value = "";
     document.getElementById('logInSpace').style.display = "block";
-    document.getElementById('afterLogIn').style.display = "none";
+    document.getElementById('homePage').style.display = "none";
+    document.getElementById('bookPage').style.display = "none";
+    document.getElementById('msgsPage').style.display = "none";
+    document.getElementById('acctPage').style.display = "none";
   }
+
+  /////////////////////////////
 
   render() {
     const { navigate } = this.props.navigation;
@@ -51,20 +118,59 @@ class HomeScreen extends React.Component {
             <Button title="Log In" onPress={ this.checkUser }/>
           </div>
         </div>
-        <div id='afterLogIn' style={{display: 'none'}}>
-          <Text id='welcomeMsg'/>
-          <Button title="Log Out" onPress={ this.logOutUser }/>
+
+        <div id='homePage' style={{display: 'none'}}>
+          <Text id='welcomeMsg'></Text>
+          <Text>This is the home page</Text>
+          <Text> </Text>
+          <div style={{display: 'block'}}>
+            <Button id='homeButton' title="Home" onPress={ this.homePageButton }/>
+            <Button id='bookButton' title="Book" onPress={ this.bookPageButton }/>
+            <Button id='msgsButton' title="Messages" onPress={ this.msgsPageButton }/>
+            <Button id='acctButton' title="Account" onPress={ this.acctPageButton }/>
+          </div>
+        </div>
+
+        <div id='bookPage' style={{display: 'none'}}>
+          <Text>This is the booking page</Text>
+          <div>
+            <Button id='homeButton' title="Home" onPress={ this.homePageButton }/>
+            <Button id='bookButton' title="Book" onPress={ this.bookPageButton }/>
+            <Button id='msgsButton' title="Messages" onPress={ this.msgsPageButton }/>
+            <Button id='acctButton' title="Account" onPress={ this.acctPageButton }/>
+          </div>
+        </div>
+
+        <div id='msgsPage' style={{display: 'none'}}>
+          <Text>This is the messages page</Text>
+          <div>
+            <Button id='homeButton' title="Home" onPress={ this.homePageButton }/>
+            <Button id='bookButton' title="Book" onPress={ this.bookPageButton }/>
+            <Button id='msgsButton' title="Messages" onPress={ this.msgsPageButton }/>
+            <Button id='acctButton' title="Account" onPress={ this.acctPageButton }/>
+          </div>
+        </div>
+
+        <div id='acctPage' style={{display: 'none'}}>
+          <Text>This is the account page</Text>
+          <Button id='logOutButton' title="Log Out" onPress={ this.logOutUser }/>
+          <div>
+            <Button id='homeButton' title="Home" onPress={ this.homePageButton }/>
+            <Button id='bookButton' title="Book" onPress={ this.bookPageButton }/>
+            <Button id='msgsButton' title="Messages" onPress={ this.msgsPageButton }/>
+            <Button id='acctButton' title="Account" onPress={ this.acctPageButton }/>
+          </div>
         </div>
       </View>
     );
   }
 }
 
-class LoggedInScreen extends React.Component {
+class CurrLocScreen extends React.Component {
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>This is a new page</Text>
+        <Text>Page to view current location on map while car is moving</Text>
       </View>
     );
   }
@@ -72,11 +178,11 @@ class LoggedInScreen extends React.Component {
 
 const RootStack = createStackNavigator(
   {
-    Home: HomeScreen,
-    LoggedIn: LoggedInScreen,
+    Main: MainScreen,
+    CurrLoc: CurrLocScreen,
   },
   {
-    initialRouteName: 'Home',
+    initialRouteName: 'Main',
   }
 );
 
