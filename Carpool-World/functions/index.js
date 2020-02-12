b5d7a61653f287ca2ff2dd1fb9d7d1bbe63947c7
Shafiq <58117391+xshafqx@users.noreply.github.com>
2020-02-13 00:25:17 +0800

    test

--- Carpool-World/functions/index.js
+++ Carpool-World/functions/index.js
@@ -1,8 +1,10 @@
 const functions = require('firebase-functions');
+const express = require('express');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+const app = express();
+
+app.get('/timestamp', (request, response) => {
+  response.send(`${Date.now()}`);
+});
+
+exports.helloWorld = functions.https.onRequest(app);
