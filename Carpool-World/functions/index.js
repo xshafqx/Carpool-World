086a5542645cc517723b6b6bd295387cd2ec1db2
Shafiq <58117391+xshafqx@users.noreply.github.com>
2020-02-13 12:16:22 +0800

    Revert "test"
    This reverts commit b5d7a61653f287ca2ff2dd1fb9d7d1bbe63947c7.

--- Carpool-World/functions/index.js
+++ Carpool-World/functions/index.js
@@ -1,10 +1,8 @@
 const functions = require('firebase-functions');
-const express = require('express');
 
-const app = express();
-
-app.get('/timestamp', (request, response) => {
-  response.send(`${Date.now()}`);
-});
-
-exports.helloWorld = functions.https.onRequest(app);
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//  response.send("Hello from Firebase!");
+// });
